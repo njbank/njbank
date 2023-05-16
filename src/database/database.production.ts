920fcf8f5e34c95d52132e28a09bc8182bf6db9b
HinanoAira <aira@hinasense.jp>
2023-05-16 17:41:58 +0900

    database.production

---
+++ src/database/database.production.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+
+import { DataSource } from 'typeorm';
+
+import { databaseEntities } from './database.module';
+
+const migrationFilesDir = 'dist/database/migrations/*.js';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  database: process.env.POSTGRES_DB,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  entities: databaseEntities,
+  migrations: [migrationFilesDir],
+  synchronize: false,
+  extra: {
+    socketPath: process.env.POSTGRES_SOCKET,
+    ssl: process.env.POSTGRES_SSL,
+  },
+});
